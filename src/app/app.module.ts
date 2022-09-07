db243f2f11aa5ce913bbc862b49175a7dfdfbb91
arham97 <54774711+arham97@users.noreply.github.com>
2022-09-07 12:50:38 +0500

    basic structure and functions implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { SquareComponent } from "./square/square.component";
+import { BoardComponent } from "./board/board.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SquareComponent, BoardComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
