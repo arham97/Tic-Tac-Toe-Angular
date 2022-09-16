6d0d3d363048a6edbe01db624e8ceee53b0d2a17
arham97 <54774711+arham97@users.noreply.github.com>
2022-09-16 11:32:15 +0500

    Implemented square and board

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,12 +2,8 @@ import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-square",
-  template: `
-    <p>
-      square works!
-    </p>
-  `,
-  styles: []
+  template: ` <button></button> `,
+  styles: ["button { width: 100%; height: 100%; font-size: 5em !important; }"]
 })
 export class SquareComponent {
   @Input() value: "X" | "O";
